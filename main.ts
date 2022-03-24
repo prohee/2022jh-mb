4caa3ac549796d59aee0b1650b42aa241319d154
prohee <95161907+prohee@users.noreply.github.com>
2022-03-24 22:01:56 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+        	
+        } else if (input.buttonIsPressed(Button.B)) {
+            basic.clearScreen()
+        }
+    }
+    basic.pause(100)
+})
