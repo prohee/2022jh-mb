4a4823aa22ce03870b8c97db0c2af28a2808e544
prohee <95161907+prohee@users.noreply.github.com>
2022-03-24 22:07:33 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+let 가위바위보 = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (!(input.buttonIsPressed(Button.AB))) {
         if (input.buttonIsPressed(Button.A)) {
-        	
+            가위바위보 = randint(0, 2)
+            if (가위바위보 == 0) {
+                basic.showIcon(IconNames.Scissors)
+            } else if (가위바위보 == 1) {
+                basic.showIcon(IconNames.SmallSquare)
+            } else {
+                basic.showIcon(IconNames.Square)
+            }
         } else if (input.buttonIsPressed(Button.B)) {
             basic.clearScreen()
         }
